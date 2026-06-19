68be2113a4a54d3b69b256e0cba8c14d3bb02856
Zeilian <josewilliandecarvalho@gmail.com>
2026-06-19 14:05:24 -0300

    te odeio

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,11 @@ extends CharacterBody2D
 @onready var animacao: AnimatedSprite2D = $animaçao
 
 
-var SPEED = 1000.0
+var SPEED = 30000.0
 var JUMP_VELOCITY = -800.0
 var velocidade_queda = Vector2(0, 75)
 var altura_inicio = 0
-var estado = 0 # 0 - Nada; 1 - Subindo; 2 - Caindo;
+var estado = 0 # 0 - Parado; 1 - Subindo; 2 - Caindo;
 var pulo_tempo = 0
 var pulo_max = 0.35
 
@@ -26,12 +26,15 @@ func _physics_process(delta: float) -> void:
 			estado = 1
 	elif estado != 2:
 		estado = 0
+		
 	# Estados no ar
 	match estado:
 		1:
 			velocity += velocidade_queda
+			animacao.play("caindo")
 		2:
 			pular(delta)
+			animacao.play("pulando")
 
 	move_and_slide()
 
@@ -39,16 +42,21 @@ func mover(delta):
 
 	# Vai para os lados
 	var direction := Input.get_axis("esquerda", "direita")
-	if direction != 0:
-		velocity.x = direction * SPEED
+	if direction:
+		velocity.x = direction * SPEED * delta
 	else:
 		velocity.x = 0
+
+	if velocity.x == 0:
+		animacao.play("parado")
+	elif velocity.x != 0:
+		animacao.play("andando")
+
 	# Ajusta a animação para o lado correto
 	if velocity.x > 0:
 		animacao.flip_h = false
 	elif velocity.x < 0:
 		animacao.flip_h = true
-	
 
 func pular2():
 	var pulin = Input.is_action_pressed("pulo")
